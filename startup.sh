a700b18f73c8e1a6d5020480282184f2ca8c25ba
Meatballs <eat_meatballs@hotmail.co.uk>
2015-07-08 11:17:50 +0100

    Fix permissions

--- startup.sh
+++ startup.sh
@@ -14,6 +14,7 @@ fi
 if (! pgrep sidekiq); then
         cd /var/www/phishing-frenzy/
         bundle exec sidekiq -d -C config/sidekiq.yml
+        chown -R www-data:www-data /var/www/phishing-frenzy/
 fi
 apachectl stop
 apachectl -DFOREGROUND
